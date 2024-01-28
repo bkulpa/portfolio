ae72ccecefc0b18af5ac81d070ae70db1da79cc6
Bartosz Kulpa <bartek.kulpa95@gmail.com>
2024-01-28 23:57:46 +0100

    rootMargin has been implemented

--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -13,7 +13,12 @@ const Home = () => {
       ([entry]) => {
         setIsTest2aVisible(entry.isIntersecting);
       },
-      { threshold: 0.35 }
+      {
+        // TODO: Znaleźć optimum, wartość threshold ustawić na około 0.7
+        // root: null,
+        // rootMargin: "-33% 0px -33% 0px",
+        threshold: 1,
+      }
     );
 
     if (test2aRef.current) {
@@ -26,7 +31,6 @@ const Home = () => {
       }
     };
   }, [test2aRef]);
-
   return (
     <div style={{ backgroundColor: isTest2aVisible ? "red" : "initial" }}>
       <SplitContainer>
@@ -49,13 +53,7 @@ const Home = () => {
           <div className="test2a" ref={test2aRef} style={{ width: "50px" }}>
             JAPPA Lorem Ipsum is simply dummy text of the printing and
             typesetting industry. Lorem Ipsum has been the industry's standard
-            dummy text ever since the 1500s, when an unknown printer took a
-            galley of type and scrambled it to make a type specimen book. It has
-            survived not only five centuries, but also the leap into electronic
-            typesetting, remaining essentially unchanged. It was popularised in
-            the 1960s with the release of Letraset sheets containing Lorem Ipsum
-            passages, and more recently with desktop publishing software like
-            Aldus PageMaker including versions of Lorem Ipsum.
+            dummy text ever since the
           </div>
         </RightContainer>
       </SplitContainer>
