d3bb06d51e5fdb4f62f6a33ebc5e5d40db843de7
Bartosz Kulpa <bartek.kulpa95@gmail.com>
2024-02-13 00:48:23 +0100

    fixed highlights

--- src/components/menu/menu.component.jsx
+++ src/components/menu/menu.component.jsx
@@ -1,43 +1,54 @@
+// Przykładowy kod, zakładając, że mamy już mechanizm do zarządzania widocznością sekcji
+
 import { useSelector } from "react-redux";
 import { useTranslation } from "react-i18next";
-
 import { MenuList, MenuElement, MenuElementLink } from "./menu.styled";
 
 const Menu = () => {
   const { t } = useTranslation();
 
+  // Przykład selektorów (przyjmujemy, że te selektory już istnieją)
   const isAboutSectionOnTheScreen = useSelector(
     (state) => state.aboutVisibility.isAboutSectionOnTheScreen
   );
-
   const isEducationSectionOnTheScreen = useSelector(
     (state) => state.educationVisibility.isEducationSectionOnTheScreen
   );
-
   const isExperienceSectionOnTheScreen = useSelector(
     (state) => state.experienceVisibility.isExperienceSectionOnTheScreen
   );
-
   const isProjectsSectionOnTheScreen = useSelector(
     (state) => state.projectsVisibility.isProjectsSectionOnTheScreen
   );
 
+  // Logika do określenia, która sekcja jest wyróżniona
+  // Wymaga dostosowania w zależności od logiki określającej, która sekcja jest aktualnie na ekranie
+  const highlightedSection = isAboutSectionOnTheScreen
+    ? "about"
+    : isExperienceSectionOnTheScreen
+    ? "experience"
+    : isEducationSectionOnTheScreen
+    ? "education"
+    : isProjectsSectionOnTheScreen
+    ? "projects"
+    : null;
+
   return (
     <MenuList>
-      <MenuElement isHighlighted={isAboutSectionOnTheScreen}>
+      <MenuElement isHighlighted={highlightedSection === "about"}>
         <MenuElementLink href="#about">{t("menu.about")}</MenuElementLink>
       </MenuElement>
-      <MenuElement isHighlighted={isExperienceSectionOnTheScreen}>
+      <MenuElement isHighlighted={highlightedSection === "experience"}>
         <MenuElementLink href="#experience">
           {t("menu.experience")}
         </MenuElementLink>
       </MenuElement>
-      <MenuElement isHighlighted={isEducationSectionOnTheScreen}>
+      <MenuElement isHighlighted={highlightedSection === "education"}>
         <MenuElementLink href="#education">
           {t("menu.education")}
         </MenuElementLink>
       </MenuElement>
-      <MenuElement isHighlighted={isProjectsSectionOnTheScreen}>
+      <MenuElement isHighlighted={highlightedSection === "projects"}>
         <MenuElementLink href="#projects">{t("menu.projects")}</MenuElementLink>
       </MenuElement>
     </MenuList>
