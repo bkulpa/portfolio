1bc6d3c335025df0d0ff16a28eb77e8045033e3c
Bartosz Kulpa <bartek.kulpa95@gmail.com>
2024-02-16 00:06:18 +0100

    head properties

--- src/components/app-helmet/app-helmet.component.jsx
+++ src/components/app-helmet/app-helmet.component.jsx
@@ -7,6 +7,14 @@ const AppHelmet = () => {
     <Helmet>
       <title>{t("page.title")}</title>
       <link rel="icon" type="image/png" href="/icon.png" />
+      <meta property="og:url" content="https://www.bkulpa.pl/" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Bartosz Kulpa" />
+      <meta property="og:description" content="Bartosz Kulpa - Portfolio" />
+      <meta
+        property="og:image"
+        content="https://drive.google.com/file/d/1-Ec_8zsCsu_wPngc30AunEHgTRQTIS2J/view?usp=drive_link"
+      />
     </Helmet>
   );
 };
