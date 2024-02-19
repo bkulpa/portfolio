68f7cfe1426adeeb69fd8c41f63ba3a4d39b79e5
Bartosz Kulpa <bartek.kulpa95@gmail.com>
2024-02-20 00:21:47 +0100

    fixed routing

--- src/index.tsx
+++ src/index.tsx
@@ -16,8 +16,8 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <AppHelmet />
       <BrowserRouter>
+        <AppHelmet />
         <App />
       </BrowserRouter>
     </Provider>
