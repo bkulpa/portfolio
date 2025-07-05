4ed90d255e36b09487762afa1c4947a65d5f72a5
Bartosz Kulpa <bartek.kulpa95@gmail.com>
2025-07-05 03:48:23 +0200

    Added text justify

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -7,7 +7,6 @@ import { CVLink } from "../experience-field/experience-field.styles";
 import Menu from "../menu/menu.component";
 import StyledH1 from "../styled-tags/styled-h1.styles";
 import StyledH2 from "../styled-tags/styled-h2.styles";
-import StyledP from "../styled-tags/styled-p.styles";
 import LanguageSwitcher from "../language-switcher/language-switcher.component";
 
 const Header = () => {
@@ -16,9 +15,6 @@ const Header = () => {
     <div style={{ marginTop: "100px" }}>
       <StyledH1>{t("home.nameAndSurname")}</StyledH1>
       <StyledH2>{t("home.jobTitle")}</StyledH2>
-      <div style={{ maxWidth: "320px", marginTop: "16px" }}>
-        <StyledP>{t("home.jobTitleDescription")}</StyledP>
-      </div>
       <LanguageSwitcher />
       <div style={{ display: "flex" }}>
         <CVLink
--- src/components/styled-tags/styled-p.styles.jsx
+++ src/components/styled-tags/styled-p.styles.jsx
@@ -4,6 +4,7 @@ const StyledP = styled.p`
   color: rgb(148 163 184);
   font-size: 16px;
   line-height: 26px;
+  text-align: justify;
 `;
 
 export default StyledP;
